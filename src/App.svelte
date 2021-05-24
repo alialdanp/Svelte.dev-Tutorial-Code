0d490e0381d5966f24b12697730da421ff7e516d
Ali Alioui <ali123plus@gmail.com>
2021-05-25 00:49:01 +0100

    02-03. Statements

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,17 @@
 <script>
   let count = 0;
-  $: doubled = count * 2;
+
+  // $: console.log(`the count is ${count}`);
+
+  // $: {
+  //   console.log(`the count is ${count}`);
+  //   alert(`I SAID THE COUNT IS ${count}`);
+  // }
+
+  $: if (count >= 10) {
+    alert(`count is dangerously high!`);
+    count = 9;
+  }
 
   function handleClick() {
     count += 1;
@@ -11,5 +22,3 @@
   Clicked {count}
   {count === 1 ? 'time' : 'times'}
 </button>
-
-<p>{count} doubled is {doubled}</p>
