7606f87f6d1aaf2f6246795e2e7145945a7879cf
Ali Alioui <ali123plus@gmail.com>
2021-05-25 23:37:51 +0100

    05-03. Event modifiers

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,13 @@
 <script>
-  let m = { x: 0, y: 0 };
+  function handleClick() {
+    alert('no more alerts');
+  }
 </script>
 
-<div on:mousemove={(e) => (m = { x: e.clientX, y: e.clientY })}>
-  The mouse position is {m.x} x {m.y}
-</div>
+<button on:click|once={handleClick}> 
+  Click me 
+</button>
 
-<style>
-  div {
-    width: 100%;
-    height: 100%;
-  }
-</style>
+<!-- You can chain modifiers together, e.g. 
+  on:click|once|capture={...}. 
+-->
