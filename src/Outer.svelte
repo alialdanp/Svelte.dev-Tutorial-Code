31ab45f4ba9d4e2f6147bdae1805b0fd5bb38008
Ali Alioui <ali123plus@gmail.com>
2021-05-26 13:55:17 +0100

    05-05. Event forwarding

---
+++ src/Outer.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Inner from './Inner.svelte';
+</script>
+
+<Inner on:message />
+
+<!-- au lieu de 
+
+<script>
+  import Inner from './Inner.svelte';
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  function forward(event) {
+    dispatch('message', event.detail);
+  }
+</script>
+
+<Inner on:message={forward} /> 
+
+-->
